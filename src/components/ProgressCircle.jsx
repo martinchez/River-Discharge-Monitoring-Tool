8eec836053a42c11046c4f6b17c7e5cd6d40d0c4
martinchez <martinchege1998@gmail.com>
2023-08-30 12:21:16 +0300

    Progress bar

--- src/components/ProgressCircle.jsx
+++ src/components/ProgressCircle.jsx
@@ -1,11 +1,10 @@
-import {Box, useTheme} from '@mui/material'
+import { Box, useTheme } from '@mui/material'
 import { tokens } from '../theme'
-i
 
-const ProgressCircle = ({progress = "0.75", size ="40"}) => {
-    const theme = useTheme();
-    const colors = tokens(theme.palette.mode)
-    const angle = progress * 360
+const ProgressCircle = ({ progress = '0.75', size = '40' }) => {
+  const theme = useTheme()
+  const colors = tokens(theme.palette.mode)
+  const angle = progress * 360
   return (
     <Box
       sx={{
